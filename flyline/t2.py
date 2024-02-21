5c16b2f8c9483f6d295ae1ec267e9f79556d4873
hsuan619 <06ashan19@gmail.com>
2024-02-21 22:06:44 +0800

    update

---
+++ flyline/t2.py
@@ -0,0 +1,172 @@
+import pygsheets
+import pandas as pd
+import schedule
+import time
+from datetime import datetime, timedelta
+
+
+auth_file = "flyline/flybot.json"
+gc = pygsheets.authorize(service_file=auth_file)
+sheet_id = "1KYsTpbF6eiTT3-GP3VFlXdiOWe8WwCa5z6ZA1XC1RKk"
+sh = gc.open_by_key(sheet_id)
+
+
+wks = sh.worksheet_by_title("course")
+wks2 = sh.worksheet_by_title("teacher")
+
+
+def setCourse(name, user):
+    # If multiple numbers are provided, split them and update the worksheet for each number
+    # num_list = num.split(",")
+
+    # for n in num_list:
+    row = (
+        len(wks2.get_col(1, returnas="cell", include_empty=False)) + 1
+    )  # Find the next available row
+    #     n = n.strip()
+    wks2.update_values(f"A{row}", [[name]])  # Add the data to the next avai
+    wks2.update_values(f"B{row}", [[user]])
+
+    # n = n.split(" ")
+    # wks2.update_values(f"C{row}", [[n[1]]])
+
+
+def getDeatilByUser(id):
+    results = wks2.find(id)
+    dt = ""
+    if results:
+        courseList = []
+        for i in results:
+            r = int(i.row)
+            name = wks2.get_value(f"A{r}")
+            # print(name)
+            target = wks.find(name)
+            for t in target:
+                r = t.row
+                dt = wks.get_value(f"A{r}") + " " + wks.get_value(f"B{r}")
+                courseList.append(dt)
+            courseList = "\n".join(courseList)
+        return courseList  # ['Speaking Part 1 & 2 01/03(三)', 'Speaking Part 2 & 3 01/05(三)']
+
+    else:
+        return False
+
+
+# def getUser(re):
+#     for r in re:
+#         target = wks.find(r)
+#         for t in target:
+#             r = int(t.row)
+#             userName = wks.get_value(f"B{r}")
+#             if wks2.find(userName):
+#                 id = wks2.find(userName)
+#                 for i in id:
+#                     r = int(i.row)
+#                     userID = wks2.get_value(f"B{r}")
+#                     return userID
+#             else:
+#                 return "not found in teacher"
+def getUser(re):
+    target = wks.find(re)
+    for t in target:
+        r = int(t.row)
+        userName = wks.get_value(f"C{r}")
+        if wks2.find(userName):
+            id = wks2.find(userName)
+            for i in id:
+                r = int(i.row)
+                userID = wks2.get_value(f"B{r}")
+                return userID
+        else:
+            return "not found in teacher"
+
+
+# print(getUser("id"))
+
+
+def delUser(id):
+    results = wks2.find(id)
+    if results:
+        for i in results:
+            r = int(i.row)
+
+            wks2.delete_rows(r, 1)  # 起始,結束
+        print("刪除成功")
+    else:
+        print("刪除失敗")
+
+
+def get_next_day(date_string: str) -> str:
+    """Return the next day as a formatted string (MM/DD)"""
+    date_object = datetime.strptime(date_string, "%m/%d")
+    next_day = date_object + timedelta(days=1)
+    return next_day.strftime("%m/%d")
+
+
+def get_pre_day(date_string: str) -> str:
+    """Return the next day as a formatted string (MM/DD)"""
+    date_object = datetime.strptime(date_string, "%m/%d")
+    pre_day = date_object - timedelta(days=1)
+    return pre_day.strftime("%m/%d")
+
+
+def getToday():
+    today = datetime.today().strftime("%m/%d")
+    return today
+
+
+def check_date_in_sheet():
+    # 檢查試算表中的日期欄位
+    try:
+        # all_values = wks2.get_all_values()
+        # delExpireRow()
+        next_day = get_next_day(getToday())
+        # print(today, next_day)
+        target = wks.find(next_day)
+        # print(f'在{target}')
+        re = []
+        if target:
+            for t in target:
+                r = t.row
+
+                re.append(wks.get_value(f"A{r}"))
+
+        else:
+            print("今天無")
+
+        return re
+    except Exception:
+        return 0
+
+
+def delExpireRow():
+    expireDate = getToday()
+    expireTarget = wks.find(expireDate)
+    if expireTarget:
+        rows = []
+        # print(expireTarget)
+        for t in expireTarget:
+            r = int(t.row)
+            # print(r)
+            wks.update_values(f"A{r}", [[""]])
+            wks.update_values(f"B{r}", [[""]])
+            wks.update_values(f"C{r}", [[""]])
+
+        # print(getToday())
+        # print("刪除過期成功")
+    else:
+        return 0
+
+
+def getDateFromCourse(course):
+    for c in wks.find(course):
+        row = int(c.row)
+        date = wks.get_value(f"B{row}")
+        return date
+
+
+def isExist(id):
+    if wks2.find(id):
+        return True
+    else:
+        return False
---
+++ flyline/views.py
@@ -0,0 +1,209 @@
+from flyline.t2 import *
+from threading import Thread
+import time
+from apscheduler.schedulers.background import BlockingScheduler, BackgroundScheduler
+from flask import Flask, request, abort
+
+# Create your views here.
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+from linebot.models import (
+    MessageEvent,
+    TextSendMessage,
+    TemplateSendMessage,
+    ButtonsTemplate,
+    QuickReply,
+    QuickReplyButton,
+    MessageTemplateAction,
+    MessageAction,
+    ConfirmTemplate,
+    PostbackEvent,
+    PostbackAction,
+    PostbackTemplateAction,
+)
+
+
+line_bot_api = LineBotApi(
+    "oPfO16vQOYT+7nHYUmUZ67tQX4FpNmEzbnd6lnLBDscB1PQ22c8Vnei2DCRIf94EhJtyFzRUjGWJ4wdikOiD+uxJ8QlULl/76r/er4XY1sUepPLLsVYmm074L/ZZx2yDtNedL6WFi5Eo8ljhVoq/jgdB04t89/1O/w1cDnyilFU="
+)
+parser = WebhookParser("88c24b64b2af3ea8b6597232821c24e5")
+
+
+def notice(Userid, detail):
+    dt = detail + " " + getDateFromCourse(detail)
+    try:
+        line_bot_api.push_message(
+            Userid,
+            TemplateSendMessage(
+                alt_text="到課提醒",
+                template=ButtonsTemplate(
+                    title="確認出席明日課程",
+                    text=f"{dt}",
+                    actions=[
+                        PostbackAction(
+                            label="我會準時到", text="我會準時到", data="準時"
+                        )
+                    ],
+                ),
+            ),
+        )
+        print("LINE NOTICE")
+    except Exception:
+        print("ERROR @ LINE NOTICE")
+
+
+def check_spreadsheet():
+    try:
+        delExpireRow()
+        numAndDate = check_date_in_sheet()
+        if numAndDate:
+            for d in numAndDate:
+                user = getUser(d)
+                notice(user, d)
+                print(f"通知{user}")
+        time.sleep(10)
+
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+        time.sleep(10)
+
+
+@csrf_exempt
+def callback(request):
+    if request.method == "POST":
+        signature = request.META["HTTP_X_LINE_SIGNATURE"]
+        body = request.body.decode("utf-8")
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            uid = event.source.user_id  # 取user id
+
+            if isinstance(event, PostbackEvent):  # 如果有postback事件
+                if event.postback.data == "teacher":
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text="請輸入您的大名（第一個字請加@）："),
+                    )
+                elif event.postback.data == "學生":
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text="目前不開放學生綁定"),
+                    )
+                if event.postback.data == "準時":
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text="感謝您的回覆"),
+                    )
+                if event.postback.data == "刪除":
+                    delUser(event.source.user_id)
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text="刪除成功"),
+                    )
+            elif isinstance(event, MessageEvent):
+                rcMsg = event.message.text
+                if "綁定中" in rcMsg:
+                    if isExist(uid) != True:
+                        line_bot_api.reply_message(
+                            event.reply_token,
+                            TemplateSendMessage(
+                                alt_text="Buttons template",
+                                template=ButtonsTemplate(
+                                    title="身分選擇",
+                                    text="請選擇綁定身分",
+                                    actions=[
+                                        PostbackTemplateAction(
+                                            label="我是學生",
+                                            text="我是student",
+                                            data="學生",
+                                        ),
+                                        PostbackTemplateAction(
+                                            label="我是老師",
+                                            text="我是老師",
+                                            data="teacher",
+                                        ),
+                                    ],
+                                ),
+                            ),
+                        )
+                    else:
+                        line_bot_api.reply_message(
+                            event.reply_token, TextSendMessage(text="資料已存在")
+                        )
+                elif "@" in rcMsg:
+                    rcMsg = rcMsg[1:]
+                    setCourse(rcMsg, uid)  # 不存在則寫入
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(
+                            text="綁定成功 ➜ 請點選圖文選單「本月課表」查看您的行程"
+                        ),
+                    )
+
+                elif "解除" in rcMsg:
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TemplateSendMessage(
+                            alt_text="Confirm template",
+                            template=ConfirmTemplate(
+                                text="您確定要解除綁定?(會一併刪除所有紀錄)",
+                                actions=[
+                                    PostbackAction(label="是", text="是", data="刪除"),
+                                    PostbackTemplateAction(
+                                        label="否", text="否", data="不刪除"
+                                    ),
+                                ],
+                            ),
+                        ),
+                    )
+
+                elif "查詢" in rcMsg:
+                    detail = getDeatilByUser(uid)
+                    if detail:  # 如果這個人的資料存在
+                        line_bot_api.reply_message(
+                            event.reply_token,
+                            TextSendMessage(text="本月您的課程時間是：\n" + detail),
+                        )
+                    else:
+                        line_bot_api.reply_message(
+                            event.reply_token,
+                            TextSendMessage(text="查無您的資料，請私訊Laura協助🧚‍♀️"),
+                        )
+                elif event.message.type == "sticker":
+                    line_bot_api.reply_message(
+                        event.reply_token, TextSendMessage(text="收到你的貼圖囉！")
+                    )
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
+
+
+import urllib
+
+scheduler = BackgroundScheduler(timezone="Asia/Shanghai")
+
+
+def scheduled_job():
+    url = "https://newfly.fly.dev"
+    urllib.request.urlopen(url)
+
+
+scheduler.add_job(
+    check_spreadsheet, "cron", hour=18, minute=00
+)  # 設定前一天的晚上6點（24小時制）
+
+scheduler.start()
+# ===============
+# 先綁定再做
+# ================
